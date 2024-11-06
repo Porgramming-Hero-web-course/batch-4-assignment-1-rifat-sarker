4c8bd3c6e241e6d4430eca6ca0d846b6e64cd1c3
Rifat Sarker <rifatswd@gmail.com>
2024-11-06 20:45:20 +0600

    problem-1 solution added

---
+++ problem-1.ts
@@ -0,0 +1,11 @@
+const sumArray = (arr: number[]): number => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i]
+        sum += element
+    }
+    return sum;
+}
+// const result = sumArray([4,1,35,5])
+// console.log(result);
+
